a2a8b808b5609739f68450c017e0f9991b5897df
Elissa <elissa.matsushita@gmail.com>
2020-01-28 17:53:35 +0000

    set up basic styling and layout, character count

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  $("textarea").on("keyup", function() {
+    const charCount = 140 - this.value.length;
+    $("form").find(".counter").text(charCount);
+    if (charCount >= 0) {
+      $("form").find(".counter").css("color", "#545149");
+    } else if (charCount < 0) {
+      $("form").find(".counter").css("color", "red");
+    }
+  })
+
+
+});
+
