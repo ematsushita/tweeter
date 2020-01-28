626cd85936abf09ae04b56a3c5caf58384e098f4
Elissa <elissa.matsushita@gmail.com>
2020-01-28 23:35:11 +0000

    added hover box shadow effect

--- public/scripts/tweets.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-  $(".tweet").hover(function() {
-    $(this).css("box-shadow", "10px 10px 10px #d3d3d3")
-  }, function() {
-    $(this).css("box-shadow", "0px 0px")
-  })
-});
