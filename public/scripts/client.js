8cf7237d4a1516a77eac080725e82683d81453a1
Elissa <elissa.matsushita@gmail.com>
2020-01-30 00:45:35 +0000

    added toggle buttons to open and focus new tweet form

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -14,41 +14,47 @@ $(document).ready(function() {
     }
   };
   
-  const createTweetElement = function (data) {
+  const createTweetElement = function ({user: {avatars, handle, name}, content, created_at}) {
     
     const $avatar = $("<img>")
-    .attr("src", data.user.avatars)
+    .attr("src", avatars)
     
     const $name = $("<div>")
-    .text(data.user.name)
+    .text(name)
     .addClass("name")
     .append($avatar);
 
     const $username = $("<div>")
-    .text(data.user.handle)
+    .text(handle)
     .addClass("username hidden");
 
     const $tweet = $("<p>")
-    .text(data.content.text)
+    .text(content.text)
     .addClass("tweet-text");
 
     const $timeStamp = $("<div>")
-    .text(standardTime(data.created_at))
+    .text(standardTime(created_at))
     .addClass("time-stamp");
 
-    const socialIcons =
-    `<div class="social-icons">
-    <i class="fas fa-heart"></i>
-    <i class="fas fa-flag"></i>
-    <i class="fas fa-retweet"></i>
-    </div>`;
+    const $socialHeart = $("<i>")
+    .addClass("fas fa-heart");
+
+    const $socialFlag = $("<i>")
+    .addClass("fas fa-flag");
+
+    const $socialTweet = $("<i>")
+    .addClass("fas fa-retweet");
+
+    const $socialIcons = $("<div>")
+    .addClass("social-icons")
+    .append($socialHeart, $socialFlag, $socialTweet);
 
     const $header = $("<header>")
     .append($name)
     .append($username);
 
     const $footer = $("<footer>")
-    .append(socialIcons, $timeStamp);
+    .append($socialIcons, $timeStamp);
 
     const $article = $("<article>")
     .addClass("tweet")
@@ -83,7 +89,36 @@ $(document).ready(function() {
     $.post("/tweets", serialData, () => {
       $("#tweets-container").prepend(loadTweets());
       $("#composeTweet")[0].reset();
+      $(".counter").text(140);
     })
+  });
+
+  $("#arrow-down").click(function() {
+    $(".new-tweet").slideToggle(500, function() {
+      $(this).find("textarea").focus();
+    });
+  });
+
+  $("#arrow-up").click(function() {
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: "smooth"
+    });
+    $(".new-tweet").slideDown(1200, function() {
+      $(this).find("textarea").focus();
+    });
+  });
+  
+
+  $(window).scroll(function(){
+    if($(document).scrollTop() > 520){
+      $(".form-toggle").css({"display": "none"});
+      $("#arrow-up").css({"display": "block"});
+    } else {
+      $(".form-toggle").css({'display': 'block'});
+      $("#arrow-up").css({"display": "none"});
+    }
   });
 
 });
