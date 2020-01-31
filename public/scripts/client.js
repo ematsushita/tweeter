6a770cd1e7736913e054a92d4f1aca8159f6d2c5
Elissa <elissa.matsushita@gmail.com>
2020-01-31 00:37:09 +0000

    added styling to background

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -14,6 +14,7 @@ $(document).ready(function() {
     }
   };
   
+  //Generate new tweets by building HTML using variables for security
   const createTweetElement = function ({user: {avatars, handle, name}, content, created_at}) {
     
     const $avatar = $("<img>")
@@ -67,6 +68,7 @@ $(document).ready(function() {
     return $article;
   };
 
+  //Prepend new tweets to the top of the tweets container to show newest tweets first
   const renderTweets = function(data) {
     for (const tweet of data) {
       const $newTweet = createTweetElement(tweet);
@@ -83,10 +85,13 @@ $(document).ready(function() {
 
   loadTweets();
 
+  //Events to fire once new tweet is submitted
   $("#compose-tweet").submit(function(event) {
     event.preventDefault();
     const serialData = $(this).serialize();
     const tweetLength = $("textarea").val().length;
+
+    //Check to see that the tweet is in between  1-140 characters, if not show error message
     if (tweetLength > 140 || tweetLength <= 0) {
       return $("#error-messages").slideDown(300);
     } else {
@@ -94,6 +99,8 @@ $(document).ready(function() {
         $("textarea").focus();
       })
     }
+
+    //Send a post request to add new tweet
     $.post("/tweets", serialData, () => {
       $("#tweets-container").prepend(loadTweets());
       $("#compose-tweet")[0].reset();
@@ -101,12 +108,15 @@ $(document).ready(function() {
     })
   });
 
+  //When the #arrow-down is clicked, the "compose tweet" form should slide down and focus
   $("#arrow-down").click(function() {
     $(".new-tweet").slideToggle(500, function() {
       $(this).find("textarea").focus();
     });
   });
 
+  //The #arrow-up is clicked, the page should scroll to the top and the "compose tweet" form 
+  //should slide down and focus
   $("#arrow-up").click(function() {
     window.scrollTo({
       top: 0,
@@ -118,7 +128,8 @@ $(document).ready(function() {
     });
   });
   
-
+  //When the user scrolls past the header, the #write-a-tweet message and #arrow-down should be hidden
+  //and the #arrow-up should appear at the bottome right corner and vice versa
   $(window).scroll(function(){
     if($(document).scrollTop() > 520){
       $("#write-a-tweet").css({"display": "none"});
